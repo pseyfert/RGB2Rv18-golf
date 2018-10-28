950a2d322b13baa2848d4070e9491d06b36f2c6d
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-10-28 22:23:26 +0100

    formatting messup. thanks @sur5r for spotting

--- duodecimal/golang/p.go
+++ duodecimal/golang/p.go
@@ -1,26 +1,14 @@
 package main
-
-import (
-	b "bufio"
-	m "fmt"
-	"os"
-	s "strconv"
-)
-
-func main() {
-	t, _ := b.NewReader(os.Stdin).ReadString('\n')
-	f, _ := s.ParseFloat(t[0:len(t)-1], 64)
-	d := s.FormatInt(int64(f*1728), 12)
-	l := len(d)
-	c := 1
-	for string(d[l-c]) == "0" && c < 4 {
-		c += 1
-	}
-	l -= 3
-	if c < 4 {
-		t = "."
-	} else {
-		t = ""
-	}
-	m.Printf("%s%s%s", d[0:l], t, d[l:l-c+4])
-}
+import (b"bufio"
+m"fmt"
+"os"
+s"strconv")
+func main(){t,_:=b.NewReader(os.Stdin).ReadString('\n')
+f,_:=s.ParseFloat(t[0:len(t)-1],64)
+d:=s.FormatInt(int64(f*1728),12)
+l:=len(d)
+c:=1
+for string(d[l-c])=="0"&&c<4{c+=1}
+l-=3
+if c<4{t="."}else{t=""}
+m.Printf("%s%s%s",d[0:l],t,d[l:l-c+4])}
